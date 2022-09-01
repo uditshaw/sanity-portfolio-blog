91bc49be8fec38b24f353d9ec1389bac0d8beadc
Udit Shaw <uditshaw17@gmail.com>
2022-09-01 16:12:25 +0530

    added blog image and meta description

--- schemas/blog.js
+++ schemas/blog.js
@@ -42,6 +42,35 @@ export default {
       ],
     },
     {
+      name: "metadesc",
+      type: "string",
+      title: "Meta description",
+    },
+    {
+      title: "Blog Image",
+      name: "blogimage",
+      type: "image",
+      options: {
+        hotspot: true, // <-- Defaults to false
+      },
+      fields: [
+        {
+          name: "caption",
+          type: "string",
+          title: "Caption",
+          options: {
+            isHighlighted: true, // <-- make this field easily accessible
+          },
+        },
+        {
+          // Editing this field will be hidden behind an "Edit"-button
+          name: "attribution",
+          type: "string",
+          title: "Attribution",
+        },
+      ],
+    },
+    {
       name: "timestamp",
       type: "datetime",
       title: "Published timestamp",
